57a4adce43c108766c32e5100f0946f3e4251a4e
Stephen Howe <stevehowe10@gmail.com>
2024-09-16 04:46:11 -0600

    sort past dates for next upcoming

--- utils/neoObjectApproach.py
+++ utils/neoObjectApproach.py
@@ -2,6 +2,7 @@ import requests
 from flask import jsonify
 from config import API_KEY
 import json
+import time
 
 def neoObject(identifier): 
     # print(identifier)
@@ -22,19 +23,21 @@ def neoObjectDataStructure(identifier):
     # print(data)
     all_approaches = []
     orbital_data = []
+    current_epoch_ms = int(time.time() * 1000)
     # Collect all close_approach_data entries
     for approach in data['close_approach_data']:
-        all_approaches.append(approach)
-    for orbit in data['orbital_data']:
-        orbital_data.append(orbit)
-        # print(orbit)
+        if approach['epoch_date_close_approach'] >  current_epoch_ms:
+            all_approaches.append(approach)
+    
+    orbital_data.append(data['orbital_data'])
+    print(orbital_data)
         
     # Sort the all_approaches list by epoch_date_close_approach
-    sorted_approaches = sorted(all_approaches, key=lambda x: x['epoch_date_close_approach'], reverse=True)
+    # sorted_approaches = sorted(all_approaches, key=lambda x: x['epoch_date_close_approach'], reverse=True)
 
     
     combined_data = {
-    "sorted_approaches": sorted_approaches,
+    "sorted_approaches": all_approaches,
     "orbital_data": orbital_data
     }
 
