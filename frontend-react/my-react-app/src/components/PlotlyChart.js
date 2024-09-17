f080d80abe8192113a68e443f727544eb4ad9cad
Stephen Howe <stevehowe10@gmail.com>
2024-09-17 06:35:59 -0600

    much bigger chart

--- frontend-react/my-react-app/src/components/PlotlyChart.js
+++ frontend-react/my-react-app/src/components/PlotlyChart.js
@@ -5,10 +5,17 @@ const PlotlyChart = ({ chartData }) => {
   // Ensure chartData is defined and has the correct structure
   const { data = [], layout = {} } = chartData || {};
 
+    // Set the desired height for the chart
+    const newLayout = {
+      ...layout,
+      height: 1000,
+      width: 1000 // Adjust this value to your desired height
+    };
+
   return (
     <Plot
       data={data}
-      layout={layout}
+      layout={newLayout}
       config={{ responsive: true }} // Optional: makes the plot responsive
     />
   );
